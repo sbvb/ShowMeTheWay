313b6d36b31c570183a322f45dcc0f085078879d
Heron Silva <heronsilva@poli.ufrj.br>
2016-01-15 21:42:43 -0200

    implements simple login/logout using email and shared preferences

--- app/src/main/java/com/newhorizon/showmetheway/LoginActivity.java
+++ app/src/main/java/com/newhorizon/showmetheway/LoginActivity.java
@@ -1,18 +1,65 @@
 package com.newhorizon.showmetheway;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    public static final String SP_FILE = "com.newhorizon.showmetheway.SP";
+
+    private EditText et_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_email = (EditText) findViewById(R.id.email);
+
+        Button sign_in = (Button) findViewById(R.id.sign_in_button);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
     }
 
-    public void login(View view) {
-        // handle login
+    private void attemptLogin() {
+        String email = et_email.getText().toString();
+
+        View focusView = null;
+        boolean cancel = false;
+
+        if (TextUtils.isEmpty(email)) {
+            et_email.setError(getString(R.string.error_field_required));
+            focusView = et_email;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            et_email.setError(getString(R.string.error_invalid_email));
+            focusView = et_email;
+            cancel = true;
+        }
+
+        if(cancel) {
+            focusView.requestFocus();
+        } else {
+            SharedPreferences sp = getSharedPreferences(SP_FILE, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("EMAIL", email);
+            editor.apply();
+
+            Intent main = new Intent(this, MainActivity.class);
+            startActivity(main);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
     }
 }
--- app/src/main/java/com/newhorizon/showmetheway/MainActivity.java
+++ app/src/main/java/com/newhorizon/showmetheway/MainActivity.java
@@ -1,13 +1,46 @@
 package com.newhorizon.showmetheway;
 
+import android.app.SharedElementCallback;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String SP_FILE = "com.newhorizon.showmetheway.SP";
+
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sp = getSharedPreferences(SP_FILE, MODE_PRIVATE);
+        email = sp.getString("EMAIL", null);
+
+        if(email == null) {
+            finish();
+            Intent login = new Intent(this, LoginActivity.class);
+            startActivity(login);
+        }
+
+        final SharedPreferences.Editor editor = sp.edit();
+        final Context ctx = this;
+
+        Button logout = (Button) findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.remove("EMAIL");
+                editor.apply();
+
+                startActivity(new Intent(ctx, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
